520cc3ba5862398936cd5512181127ad702a06bc
Ankeet Maini <ankeet.maini@gmail.com>
2018-05-29 16:05:45 +0530

    Adds API calling code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "regenerator-runtime/runtime";
 
-const App = () => <h1>Hello, World</h1>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      locations: []
+    };
+    this.fetchLocations = this.fetchLocations.bind(this);
+  }
+  componentDidMount() {}
+
+  async fetchLocations(event) {
+    const val = event.target.value;
+
+    const locations = await fetch(
+      `https://developers.zomato.com/api/v2.1/locations?query=${val}`,
+      {
+        headers: {
+          "user-key": "a891e3e4af4044d83dbb53e26c28f876"
+        }
+      }
+    ).then(r => r.json());
+    this.setState({
+      locations: locations.location_suggestions
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Lomato!</h1>
+        <input
+          type="text"
+          placeholder="enter location"
+          onChange={this.fetchLocations}
+        />
+        <div />
+        list of locations
+        {this.state.locations.map(l => (
+          <div>
+            {l.city_name}, {l.country_name}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
